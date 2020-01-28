db8266aef158b3a399c8f4dcb3edc72d1fd1b7ce
Leon Woestenberg <leon@sidebranch.com>
2020-01-28 23:51:39 +0100

    Few fixes tested only on first run.

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
+#!/bin/bash
 set -e
+set -e -o pipefail
 
 function super_prove () {
-	hg clone https://bitbucket.org/sterin/super_prove_build d7b71160dddb || true
+	hg clone -r d7b71160dddb https://bitbucket.org/sterin/super_prove_build || true
 	cd super_prove_build
 	mkdir -p build
 
 	cd abc-zz
 	wget -nc "https://bitbucket.org/sterin/super_prove_build/issues/attachments/4/sterin/super_prove_build/1565269491.41/4/fix_super_prove_build.txt"
 	patch -p1 -N -i fix_super_prove_build.txt || true
-	cd ..
+	cd .. # abc-zz
 
 	cd build
 	cmake -DCMAKE_BUILD_TYPE=Release -G Ninja ..
@@ -19,13 +21,15 @@ function super_prove () {
 	# install
 	mkdir -p ~/opt/formal
 	tar xzf super_prove*.tar.gz -C ~/opt/formal/
-	cd ..
+	cd .. # build
 
 	# install wrapper
 	mkdir -p $HOME/opt/formal/bin
 	wget -nc -O $HOME/opt/formal/bin/suprove https://bitbucket.org/sterin/super_prove_build/issues/attachments/4/sterin/super_prove_build/1565269491.6/4/suprove
 	chmod +x $HOME/opt/formal/bin/suprove
 	sed -i 's@/usr/local@$HOME/opt/formal@' $HOME/opt/formal/bin/suprove
+
+	cd .. # super_prove_build
 }
 
 function extavy () {
@@ -37,14 +41,14 @@ function extavy () {
 	make -j8
 	mkdir -p $HOME/opt/formal/bin
 	cp avy/src/{avy,avybmc} $HOME/opt/formal/bin
-	cd ..
-	cd ..
+	cd .. # build
+	cd .. # extavy
 }
 
-rm -rf $HOME/opt/formal
-mkdir -p $HOME/opt/formal
+#rm -rf $HOME/opt/formal
+#mkdir -p $HOME/opt/formal
 
-super_prove
+#super_prove
 extavy
 
 cd yosys
@@ -64,11 +68,8 @@ make install
 cd ..
 
 cd z3
-#python scripts/mk_make.py
+python scripts/mk_make.py
 cd build
 make -j8
 make install DESTDIR=$HOME/opt/formal
-cd ..
-
-# assemble a PATH to all installed executables
-EXTRA_PATH=`dirname `find ~/opt/formal/ -type f -executable -print` | sort | uniq | tr '\012' ':' | head -c -1`
+cd .. #z3
--- source-setup.sh
+++ source-setup.sh
@@ -1,4 +1,6 @@
 # assemble a PATH to all installed executables
 EXTRA_PATH=`find ~/opt/formal/ -type f -executable -print | xargs -L1 dirname | sort | uniq | tr '\012' ':' | head -c -1`
 
-export PATH=$PATH:$EXTRA_PATH
+echo "Adding $EXTRA_PATH to $$PATH".
+
+#export PATH=$PATH:$EXTRA_PATH
